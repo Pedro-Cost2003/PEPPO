7c72a8aca011a1b5fc2d7db903347867a2bf6124
Pedro Henrique Costa <127677877+Pedro-Cost2003@users.noreply.github.com>
2024-08-17 22:53:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+window.animacao = ScrollReveal({ reset: true });
+
+animacao.reveal('.foto-chefe', {
+    origin:'left',
+    duration: 2000,
+    distance: '60%',
+    delay: 200
+})
+
+animacao.reveal('.prato1', {
+    origin:'right',
+    duration: 2000,
+    distance: '200%',
+    delay: 500
+})
+
+animacao.reveal('.prato2', {
+    origin:'right',
+    duration: 2200,
+    distance: '200%',
+    delay: 1000
+})
+
+animacao.reveal('.prato3', {
+    origin:'right',
+    duration: 2400,
+    distance: '200%',
+    delay: 1500
+})
+
+animacao.reveal('.text-animation1', {
+    origin:'right',
+    duration: 2000,
+    distance: '100%',
+    delay: 1000
+})
+
+animacao.reveal('.text-animation2', {
+    origin:'right',
+    duration: 2000,
+    distance: '100%',
+    delay: 1500
+})
+
+animacao.reveal('.text-animation3', {
+    origin:'right',
+    duration: 2000,
+    distance: '200%',
+    delay: 1800
+})
+
+// aqui vai ser meio da animaçoes //
+
+animacao.reveal('.text-foto-gourmet', {
+    origin:'left',
+    duration: 2500,
+    distance: '60%',
+    delay: 1000
+})
+
+animacao.reveal('.imgGurmet', {
+    origin: 'right',
+    duration: 2000,
+    distance: '50%',
+    delay: 500
+})
+
+animacao.reveal('.paragrafo-fotogourmet', {
+    origin:'left',
+    duration: 2000,
+    distance: '60%',
+    delay: 500
+})
+
+animacao.reveal('.img-carnegourmet', {
+    origin: 'right',
+    duration: 2500,
+    distance: '60%',
+    delay: 500
+})
+
+animacao.reveal('.img-chefe-gourmet', {
+    origin: 'left',
+    duration: 2500,
+    distance: '60%',
+    delay: 500
+})
+
+animacao.reveal('.textfinal', {
+    duration: 2500,
+    distance: '60%',
+    delay: 500
+})
